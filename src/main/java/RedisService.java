412b7e99ce55e946acaec5bd776f6f27eb60cb05
huangtaowork <361299045@qq.com>
2018-08-22 13:13:32 +0800

    fix

--- src/main/java/RedisService.java
+++ src/main/java/RedisService.java
@@ -2,7 +2,9 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
-import java.util.function.Consumer;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class RedisService {
     private static String host = "127.0.0.1";
@@ -18,9 +20,34 @@ public class RedisService {
         jedisPool = new JedisPool(poolConfig, host, port);
     }
 
-    public static void withRedis(Consumer<Jedis> consumer){
+    public static String getString(String key){
+        String value;
         try(Jedis jedis = jedisPool.getResource()){
-            consumer.accept(jedis);
+            value = jedis.get(key);
         }
+        return value;
+    }
+
+
+    public static void setString(String key, String value){
+        try(Jedis jedis = jedisPool.getResource()){
+            jedis.set(key, value);
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        List<String> list = new ArrayList();
+        for(int i=0;i<10;i++){
+            list.add("id"+i);
+        }
+        String key = "flow-1::action-1";
+        int index = 0;
+        String val = RedisService.getString(key);
+        if(val != null){
+            index = Integer.parseInt(val);
+        }
+        RedisService.setString(key, String.valueOf((index + 1)));
+
+        System.out.println("index=" + index);
     }
 }
